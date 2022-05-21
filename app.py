a9e71ac7b252f1a8fd9bf5c694fcdc0ba7497855
Mike Howles <mike.howles@gmail.com>
2022-05-21 01:25:07 +0000

    more updates

---
+++ app.py
@@ -0,0 +1,98 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from dotenv import load_dotenv
+from yaml import dump, full_load
+from werkzeug.utils import secure_filename
+import hashlib
+from datetime import datetime
+
+# load_dotenv()
+
+UPLOAD_FOLDER = 'images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/register/<agent_id>')
+def register(agent_id):
+    try:
+        with open("agents.yaml", "r") as queue:
+            arr = full_load(queue)
+    except:
+        arr = []
+    found = False
+    for agent in arr:
+        if agent["agent_id"] == agent_id:
+            found = True
+            
+    if not found:
+        salt = "SaltyBoi"
+        token = hashlib.sha256(f"{agent_id}{salt}".encode('utf-8')).hexdigest()
+        arr.append({
+            "agent_id" : agent_id,
+            "last_seen" : datetime.now()
+        })
+        dump(arr, open("agents.yaml", "w"))
+        return f"✅ Registered!  Your API token is '{token}'.  Save this, you won't see it again."
+    else:
+        return f"😓 Sorry, someone already registered an agent by that name.  Try another one!"
+
+@app.route('/upload/<agent_id>/<job_uuid>', methods=['GET', 'POST'])
+def upload_file(agent_id, job_uuid):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            with open("queue.yaml", "r") as queue:
+                arr = full_load(queue)
+                for job in arr:
+                    if job["uuid"] == job_uuid:
+                        job["status"] = "complete"
+                        print("found")
+            return f"thank you, {agent_id}."
+            # return redirect(url_for('download_file', name=filename))
+    else:
+        return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+        <input type=file name=file>
+        <input type=submit value=Upload>
+        </form>
+        '''
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/takeorder/<agent_id>")
+def takeorder(agent_id):
+    with open("queue.yaml", "r") as queue:
+        arr = full_load(queue)
+    if(len(arr)) > 0:
+        job = arr[0]
+        job["status"] = "processing"
+        job["agent"] = agent_id
+        dump(arr, open("queue.yaml", "w"))
+        return str(job)
+    else:
+        return str({})
+@app.route("/queue")
+def q():
+    with open("queue.yaml", "r") as queue:
+        arr = full_load(queue)
+    return str(arr)
--- bot.py
+++ bot.py
@@ -1,11 +1,39 @@
 import discord, os, subprocess
+from discord.ext import tasks
 from dotenv import load_dotenv
+from numpy import full
 from pydotted import pydot
 from loguru import logger
-from profanity_check import predict, predict_prob
-import joblib
+import time
+from yaml import dump, full_load
+import uuid
+
+import warnings
+
+warnings.filterwarnings("ignore")
+from profanity_check import predict_prob
 
 load_dotenv()
+
+agents = ["mike"]
+ticks = 0
+
+# this code will be executed every 10 seconds after the bot is ready
+@tasks.loop(seconds=10)
+async def task_loop():
+    global ticks
+    ticks += 1
+
+    print(ticks)
+    channel = discord.utils.get(bot.get_all_channels(), name="general")
+    # agents = open("agents.txt","r").read()
+    # if agents != oldagents:
+    #   await channel.send("New render agent found.")
+
+    # oldagents = agents
+    print(ticks)
+    # await channel.send("tick")
+
 
 bot = discord.Bot(debug_guilds=[945459234194219029])  # specify the guild IDs in debug_guilds
 arr = []
@@ -23,6 +51,7 @@ async def ping(ctx):  # a slash command will be created with the name "ping"
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
+    task_loop.start()  # important to start the loop
 
 
 @bot.event
@@ -58,8 +87,16 @@ async def render(
     reject = False
     reasons = []
     authorCount = 0
+    try:
+        with open("queue.yaml", "r") as queue:
+            arr = full_load(queue)
+    except:
+        print("Empty queue file found.  Initializing...")
+        arr = []
+
     for job in arr:
-        if job.author == ctx.author:
+        job = pydot(job)
+        if job.author == str(ctx.author):
             authorCount += 1
     if authorCount >= AUTHOR_LIMIT:
         reject = True
@@ -72,7 +109,9 @@ async def render(
         reject = True
         reasons.append(f"- ❌ Profanity detected.  Watch your fucking mouth.")
     if not reject:
-        arr.append(pydot({"text_prompt": text_prompt, "steps": steps, "author": ctx.author, "status": "queued"}))
+        job_uuid = str(uuid.uuid4())
+        arr.append({"uuid": job_uuid, "text_prompt": text_prompt, "steps": steps, "author": str(ctx.author), "status": "queued"})
+        dump(arr, open("queue.yaml", "w"))
         await ctx.respond(f"✅ Request added to list")
     else:
         await ctx.respond("\n".join(reasons))
@@ -83,24 +122,54 @@ async def render(
 #     arr.append(command)
 #     res = subprocess.run(command.split(" "), stdout=subprocess.PIPE).stdout.decode("utf-8")
 #     await ctx.respond(f"```\n{res}\n```")
+@bot.command()
+async def remove(ctx, uuid):
+    author = str(ctx.author)
+    with open("queue.yaml", "r") as queue:
+        arr = full_load(queue)
+    for j, job in enumerate(arr):
+        if job["uuid"] == uuid:
+            if job["author"] == author:
+                del arr[j]
+                dump(arr, open("queue.yaml", "w"))
+                await ctx.respond(f"🗑️ Job removed.")
+            else:
+                await ctx.respond(f"❌ You are not {job['author']}!")
+    await ctx.respond(f"❌ Job not found.")
 
 
 @bot.command()
 async def queue(ctx):
+    try:
+        with open("queue.yaml", "r") as queue:
+            arr = full_load(queue)
+    except:
+        print("Empty queue file found.  Initializing...")
+        arr = []
     # https://docs.pycord.dev/en/master/api.html?highlight=embed#discord.Embed
     embed = discord.Embed(
         title="Request Queue",
         description="The following requests are queued up.",
         color=discord.Colour.blurple(),  # Pycord provides a class with default colors you can choose from
     )
-    # https://www.markdownguide.org/tools/discord/
-    md = f"""
-    - some_request
-      - some_prompts
-      - steps
-    """
     for j, job in enumerate(arr):
-        embed.add_field(name=j, value=f"- Author: `{job.author}`\n - Text Prompt: `{job.text_prompt}`", inline=False)
+        job = pydot(job)
+        summary = f"- Author: `{job.author}`\n - Text Prompt: `{job.text_prompt}`\n - Status: {job.status}"
+        embed.add_field(name=job.uuid, value=summary, inline=False)
+    await ctx.respond(embed=embed)
+
+
+@bot.command()
+async def agents(ctx):
+    # https://docs.pycord.dev/en/master/api.html?highlight=embed#discord.Embed
+    embed = discord.Embed(
+        title="Agent Status",
+        description="The following agents are running",
+        color=discord.Colour.blurple(),  # Pycord provides a class with default colors you can choose from
+    )
+    agents = open("agents.txt", "r").read().split("\n")
+    for a, agent in enumerate(agents):
+        embed.add_field(name=a, value=f"- {agent}", inline=False)
     await ctx.respond(embed=embed)
 
 
@@ -108,37 +177,64 @@ async def queue(ctx):
 async def test(ctx):
     # https://docs.pycord.dev/en/master/api.html?highlight=embed#discord.Embed
     embed = discord.Embed(
-        title="My Amazing Embed",
-        description="Embeds are super easy, barely an inconvenience.",
+        title="Job #0",
+        description="Some Text Prompts here?",
         color=discord.Colour.blurple(),  # Pycord provides a class with default colors you can choose from
     )
 
-    embed.add_field(name="A Normal Field", value="A really nice field with some information. **The description as well as the fields support markdown!**")
+    # embed.add_field(name="A Normal Field", value="A really nice field with some information. **The description as well as the fields support markdown!**")
 
-    embed.add_field(name="Inline Field 1", value="Inline Field 1", inline=True)
-    embed.add_field(name="Inline Field 2", value="Inline Field 2", inline=True)
-    embed.add_field(name="Inline Field 3", value="Inline Field 3", inline=True)
+    # embed.add_field(name="Inline Field 1", value="Inline Field 1", inline=True)
+    # embed.add_field(name="Inline Field 2", value="Inline Field 2", inline=True)
+    # embed.add_field(name="Inline Field 3", value="Inline Field 3", inline=True)
 
-    embed.set_footer(text="Footer! No markdown here.")  # footers can have icons too
+    # embed.set_footer(text="Footer! No markdown here.")  # footers can have icons too
     embed.set_author(
         name="Fever Dream",
         icon_url="https://cdn.howles.cloud/icon.png",
     )
+
+    view = discord.ui.View()
+
+    async def loveCallback(interaction):
+        await interaction.response.edit_message(content="💖", view=view)
+
+    async def hateCallback(interaction):
+        await interaction.response.edit_message(content="😢", view=view)
+
+    loveButton = discord.ui.Button(label="Love it", style=discord.ButtonStyle.green, emoji="😍")
+    loveButton.callback = loveCallback
+    hateButton = discord.ui.Button(label="Hate it", style=discord.ButtonStyle.danger, emoji="😢")
+    hateButton.callback = hateCallback
+    view.add_item(loveButton)
+    view.add_item(hateButton)
+    file = discord.File("prevFrame.png", filename="image.png")
+    embed.set_image(url="attachment://image.png")
+
+    # ar = discord.ActionRow([discord.ui.button(label="Love it", style=discord.ButtonStyle.primary)])
+    # embed.append_field(ar)
     # embed.set_thumbnail(url="https://example.com/link-to-my-thumbnail.png")
     # embed.set_image(url="https://example.com/link-to-my-banner.png")
 
-    await ctx.respond("Hello! Here's a cool embed.", embed=embed)  # Send the embed with some text
+    await ctx.respond(embed=embed, file=file, view=view)  # Send the embed with some text
 
 
-class View(discord.ui.View):  # Create a class called View that subclasses discord.ui.View
-    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary)  # Create a button with the label "😎 Click me!" with color Blurple
-    async def button_callback(self, button, interaction):
-        await interaction.response.send_message("You clicked the button!")  # Send a message when the button is clicked
+# class View(discord.ui.View):  # Create a class called View that subclasses discord.ui.View
+#     @discord.ui.button(label="Love it", style=discord.ButtonStyle.primary)  # Create a button with the label "😎 Click me!" with color Blurple
+#     @discord.ui.button(label="Hate it", style=discord.ButtonStyle.primary)  # Create a button with the label "😎 Click me!" with color Blurple
+#     async def button_callback(self, button, interaction):
+#         await interaction.response.send_message("You clicked the button!")  # Send a message when the button is clicked
 
 
 @bot.slash_command()  # Create a slash command
 async def button(ctx):
-    await ctx.respond("This is a button!", view=View())  # Send a message with our View class that contains the button
+    # f = open('prevFrame.png','rb')
+    # data = f.read()
+    await ctx.respond("Test Image", file=discord.File(open("prevFrame.png", "rb"), filename="prevFrame.png"))
 
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+if __name__ == "__main__":
+    print(discord.__version__)
+    from discord.ext import tasks, commands
+
+    bot.run(os.getenv("DISCORD_TOKEN"))
